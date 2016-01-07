e5b502a0a0e67b6391df3f5c90363439d4ee119e
Javier Quiroz <javier.s.quiroz@gmail.com>
2016-01-07 13:23:37 -0800

    seperated nav to own component. added click handlers that control river state

--- src/components/MainNav.react.js
+++ src/components/MainNav.react.js
@@ -1,36 +1,40 @@
 var React = require('react');
 
 var MainNav = React.createClass({
-
+  getInitialState: function() {
+   return {clicked: false};
+  },
   render: function() {
     return (
-      <h2>hello from the nav</h2>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">River Informer</a>
+          </div>
+
+
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li><a href="#clackamas" onClick={this.handleClick.bind(this, 'clackamas')}>Clackamas</a></li>
+              <li><a href="#sandy" onClick={this.handleClick.bind(this, 'sandy')}>Sandy</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   },
+  handleClick: function(river) {
+    this.setState({clicked: !this.state.clicked})
+    this.props.selectRiver(river)
+    console.log(river)
+  }
 });
 
 module.exports = MainNav;
-// <header>
-//   <nav class="navbar navbar-default">
-//     <div class="container-fluid">
-//       <!-- Brand and toggle get grouped for better mobile display -->
-//       <div class="navbar-header">
-//         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-//           <span class="sr-only">Toggle navigation</span>
-//           <span class="icon-bar"></span>
-//           <span class="icon-bar"></span>
-//           <span class="icon-bar"></span>
-//         </button>
-//         <a class="navbar-brand" href="#">River Informer</a>
-//       </div>
-//
-//       <!-- Collect the nav links, forms, and other content for toggling -->
-//       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-//         <ul class="nav navbar-nav">
-//           <li><a href="#">Clackamas</a></li>
-//           <li><a href="#">Sandy</a></li>
-//         </ul>
-//       </div><!-- /.navbar-collapse -->
-//     </div><!-- /.container-fluid -->
-//   </nav>
-//   </header>
--- src/components/RI_App.react.js
+++ src/components/RI_App.react.js
@@ -1,18 +1,27 @@
 var React = require('react');
-var MainNav = require('./MainNav.react')
+var MainNav = require('./MainNav.react');
+var River = require('./River.react');
 
 
 var RI_App = React.createClass({
-
+  getInitialState: function() {
+    return {river: 'clackamas'}
+  },
   render: function() {
     return (
-      <div>
-        <h1>Hello</h1>
-        <MainNav />
-      </div>
+      <section>
+        <MainNav selectRiver={this.setRiver}/>
+        <River riverName={this.state.river}/>
+      </section>
 
     );
   },
+  setRiver: function(river) {
+    this.setState({river: river})
+    console.log(river + ' passed to app')
+  }
+
+
 });
 
 module.exports = RI_App;
