46da17ed5e50fbb621cf73dc24231bbe488b6535
Javier Quiroz <javier.s.quiroz@gmail.com>
2016-01-08 20:28:27 -0800

    created helper for formatting usgs response with test coverage

--- src/usgs/helper.js
+++ src/usgs/helper.js
@@ -2,6 +2,31 @@ var _ = require('lodash');
 
 var USGS = {
 
+  simplify: function(data) {
+    data = data.value
+    var metaDataSource = data.timeSeries[0].sourceInfo;
+    var obj = {}
+    obj.siteName = metaDataSource.siteName.toLowerCase();
+    obj.siteCode = metaDataSource.siteCode[0].value;
+    obj.geoLocation = metaDataSource.geoLocation.geogLocation;
+    obj.parameters = this.setDataParameters(data.timeSeries);
+    return obj
+  },
+
+  setDataParameters: function(tsArray){
+    var parameters = [];
+    tsArray.map(function(tsItem) {
+      var param = {}
+      param.description = tsItem.variable.variableDescription;
+      param.units = tsItem.variable.unit.unitAbbreviation;
+      param.data = tsItem.values[0].value.map(function(val, ind) {
+        return {dateTime: val.dateTime, value: val.value}
+      })
+      parameters.push(param)
+    })
+    return parameters;
+  },
+
 }
 
 module.exports = USGS;
--- test/usgsData-test.js
+++ test/usgsData-test.js
@@ -1,12 +1,82 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var expect = chai.expect;
-var USGS = require('./usgs/helper');
+var USGS = require('../src/usgs/helper');
 
 chai.use(chaiHttp);
 
-describe('USGS Instantaneous Data', function() {
+var USGS_BASE_URL = 'http://waterservices.usgs.gov'
+
+describe('USGS Instantaneous Data -- single site', function() {
+
+  describe('#data', function() {
+    var usgsData = null;
+
+    before(function(done) {
+      chai.request(USGS_BASE_URL).get('/nwis/iv/?format=json&parameterCd=00060,00065,00010&period=P7D&sites=14210000')
+      .then(function(res) {
+        usgsData = JSON.parse(res.text);
+        done();
+      })
+    })
+
+    it('should be an array',function() {
+      expect(usgsData).to.be.an('object')
+    })
+  })
+
   describe('#simplify', function() {
+    var usgsData = null;
+    var simpleData = null;
+    var firstParam = null;
+
+    before(function(done) {
+      chai.request(USGS_BASE_URL).get('/nwis/iv/?format=json&parameterCd=00060,00065,00010&period=P7D&sites=14210000')
+      .then(function(res) {
+        usgsData = JSON.parse(res.text)
+        simpleData = USGS.simplify(usgsData)
+        firstParam = simpleData.parameters[0]
+        done();
+      })
+    });
+
+    describe('--object properties', function(){
+      it('should have properly formatted name of site', function() {
+        expect(simpleData).to.have.property('siteName').and.eql('clackamas river at estacada, or')
+      });
+      it('should have a siteCode', function() {
+        expect(simpleData).to.have.property('siteCode').and.eql('14210000')
+      });
+      it('should have site geodata', function() {
+        expect(simpleData).to.have.property('geoLocation')
+          .and.to.be.an('object')
+          .and.to.have.property('longitude')
+      });
+      it('should have parameters', function() {
+        expect(simpleData).to.have.property('parameters').and.length(3)
+      })
+    });
+
+    describe('--parameters', function() {
+
+
+      it('should each have description', function() {
+        expect(firstParam).to.have.property('description')
+          .and.to.eql('Temperature, water, degrees Celsius')
+      });
+      it('should each have properly formated units', function() {
+        expect(firstParam).to.have.property('units')
+          .and.to.eql('deg C')
+      });
+      it('should each have collection of data', function() {
+        expect(firstParam).to.have.property('data')
+          .and.is.an('array');
+        expect(firstParam.data[0]).to.have.property('value')
+          .and.to.be.ok;
+        expect(firstParam.data[0]).to.have.property('dateTime')
+          .and.to.be.ok;
+      });
+    })
 
   })
 });
